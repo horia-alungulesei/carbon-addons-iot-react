aad8658f08d23c7176ba0234d1cddd6339a85a35
Bilal Al-Saeedi <bilal.alsaeedi@de.ibm.com>
2019-09-19 15:08:53 +0200

    Support carbon donut chart

--- src/components/DonutCard/DonutCard.jsx
+++ src/components/DonutCard/DonutCard.jsx
@@ -1,62 +1,127 @@
 import React from 'react';
+import { DonutChart } from '@carbon/charts-react';
+import '@carbon/charts/style.css';
+import isEmpty from 'lodash/isEmpty';
 import styled from 'styled-components';
-import C3Chart from 'react-c3js';
-import 'c3/c3.css';
+import withSize from 'react-sizeme';
 
-import { DonutCardPropTypes, CardPropTypes } from '../../constants/PropTypes';
+import { CardPropTypes, DonutCardPropTypes } from '../../constants/PropTypes';
 import { CARD_SIZES } from '../../constants/LayoutConstants';
 import Card from '../Card/Card';
 
-const ContentWrapper = styled.div`
-  margin: 0 16px 16px 16px;
-  padding-bottom: 8px;
+const DonutChartWrapper = styled.div`
+  padding-left: 16px;
+  padding-right: 1rem;
+  padding-top: ${props => (props.isLegendHidden ? '16px' : '0px')};
+  padding-bottom: ${props => (!props.size === CARD_SIZES.MEDIUM ? '16px' : '0px')};
+  position: absolute;
+  width: 100%;
+  height: ${props => props.contentHeight};
+
+  &&& {
+    .chart-wrapper g.x.axis g.tick text {
+      transform: rotateY(0);
+      text-anchor: initial !important;
+    }
+    .expand-btn {
+      display: ${props => (props.isEditable ? 'none' : '')};
+    }
+    .legend-wrapper {
+      display: ${props => (props.isLegendHidden ? 'none' : '')};
+      height: ${props => (!props.size === CARD_SIZES.MEDIUM ? '40px' : '20px')} !important;
+      margin-top: -10px;
+      padding-right: 20px;
+    }
+    .chart-holder {
+      width: 100%;
+      height: 100%;
+    }
+    .chart-svg {
+      width: 100%;
+      height: 100%;
+      margin-top: ${props => (props.isLegendHidden ? '-10px' : '')};
+      circle.dot {
+        stroke-opacity: ${props => (props.isEditable ? '1' : '')};
+      }
+    }
+    .chart-tooltip {
+      display: ${props => (props.isEditable ? 'none' : '')};
+    }
+  }
 `;
 
-const DonutCard = ({ title, content, content: { data }, size, ...others }) => {
-  const chart = {
-    data: {
-      columns: data.map(i => [i.label, i.value]),
-      colors: data.reduce(
-        (acc, curr) => Object.assign({}, acc, curr.color ? { [curr.label]: curr.color } : {}),
-        {}
-      ),
-      type: 'donut',
-    },
-    donut: {
-      title: content.title,
-      label: {
-        format: value => value,
-      },
-    },
-    legend: {
-      position: 'right',
-    },
-    tooltip: {
-      format: {
-        value: value => value,
-      },
-    },
-    padding: {
-      top: 10,
-      right: 20,
-      left: 20,
-    },
-  };
+const determineHeight = (size, measuredWidth) => {
+  let height = '100%';
+  switch (size) {
+    case CARD_SIZES.MEDIUM:
+    case CARD_SIZES.LARGE:
+      if (measuredWidth && measuredWidth > 635) {
+        height = '90%';
+      }
+      break;
+    case CARD_SIZES.XLARGE:
+      height = '90%';
+      break;
+    default:
+      break;
+  }
+  return height;
+};
+
+const DonutChartCard = ({
+                          title,
+                          content: { accessibility },
+                          size,
+                          interval,
+                          isEditable,
+                          values: chartData,
+                          locale,
+                          ...others
+                        }) => {
   return (
-    <Card title={title} size={size} {...others}>
-      {!others.isLoading ? (
-        <ContentWrapper>
-          <C3Chart {...chart} style={{}} />
-        </ContentWrapper>
-      ) : null}
-    </Card>
+    <withSize.SizeMe>
+      {({ size: measuredSize }) => {
+        const height = determineHeight(size, measuredSize.width);
+        return (
+          <Card
+            title={title}
+            size={size}
+            {...others}
+            isEditable={isEditable}
+            isEmpty={isEmpty(Object.keys(chartData))}
+          >
+            {!others.isLoading && !isEmpty(Object.keys(chartData)) ? (
+              <DonutChartWrapper
+                size={size}
+                contentHeight={height}
+                isLegendHidden={Object.keys(chartData).length === 1}
+                isEditable={isEditable}
+              >
+                <DonutChart
+                  data={chartData}
+                  options={{
+                    animations: false,
+                    accessibility: accessibility || false,
+                    legendClickable: !isEditable,
+                    containerResizable: true,
+                  }}
+                  width="100%"
+                  height="100%"
+                />
+              </DonutChartWrapper>
+            ) : null}
+          </Card>
+        );
+      }}
+    </withSize.SizeMe>
   );
 };
 
-DonutCard.propTypes = { ...CardPropTypes, ...DonutCardPropTypes };
+DonutChartCard.propTypes = { ...CardPropTypes, ...DonutCardPropTypes };
 
-DonutCard.defaultProps = {
+DonutChartCard.defaultProps = {
   size: CARD_SIZES.MEDIUM,
+  values: {},
 };
 
-export default DonutCard;
+export default DonutChartCard;
